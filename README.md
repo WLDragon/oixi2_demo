728d274177d1d3303685121893dd3292e09e3636
WLDragon <cwloog@qq.com>
2021-09-05 12:20:42 +0800

    去掉onload，微信小游戏不能模拟onload

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
 # 《小迷宫大逃亡》
 
-  这是开源项目`oixi2`的示例工程
+这是开源项目`oixi2`的示例工程
 
-  展示如何使用`oixi2`和`pixi.js`以及`vite`搭建一个小游戏项目
+展示如何使用`oixi2`和`pixi.js`以及`vite`搭建一个小游戏项目
 
+## 浏览器体验
 
-## 微信小游戏体验地址
+[点击访问案例](https://wldragon.github.io/rat-in-maze/)
 
-  ![image](https://github.com/WLDragon/oixi2_demo/blob/main/qrcode.jpg)
+## 微信小游戏体验
 
+![image](https://github.com/WLDragon/oixi2_demo/blob/main/qrcode.jpg)
 
 ## 玩法
 
-  在直线上点击屏幕掉头，在路口点击屏幕选择方向，找到出口为胜利，被猫追上则失败
+在直线上点击屏幕掉头，在路口点击屏幕选择方向，找到出口为胜利，被猫追上则失败
 
+## NPM 脚本
 
-## npm script
+- `npm install`
 
-  - `npm install`
+- `npm dev` 运行调试环境
 
-  - `npm dev` 运行调试环境
+- `npm build` 构建生产环境代码
 
-  - `npm build` 构建生产环境代码
+- `npm serve` 运行生产环境
 
-  - `npm serve` 运行生产环境
+- `npm image` 将_assist/images里的图片资源写到imageDebug.js，方便调试
 
-  - `npm image` 将_assist/images里的图片资源写到imageDebug.js，方便调试
+## 生产环境图片集
+
+使用[ShoeBox](http://renderhjs.net/shoebox/)生成图片集
 
 
--- main.ts
+++ main.ts
@@ -23,45 +23,42 @@ settings.STRICT_TEXTURE_CACHE = true
 setTextDefaultStyle({ fill: 0xffffff, fontSize: 36, fontFamily: ['sans-serif'] })
 setLayoutSize(WIDTH, HEIGHT)
 
-window.onload = function () {
-  initI18n(i18nConfig)
-  storage.init()
+initI18n(i18nConfig)
+storage.init()
 
-  let application = new Application({
-    width: WIDTH,
-    height: HEIGHT,
-    sharedLoader: true,
-    sharedTicker: true,
-    backgroundColor: COLOR_LIGHT_ORANGE,
-    resolution: window.devicePixelRatio || 1
-  })
+let application = new Application({
+  width: WIDTH,
+  height: HEIGHT,
+  sharedLoader: true,
+  sharedTicker: true,
+  backgroundColor: COLOR_LIGHT_ORANGE,
+  resolution: window.devicePixelRatio || 1
+})
 
-  //适配ipad，两边留白
-  let w = window.innerWidth
-  let h = window.innerHeight
-  if (w / h > 0.6) {
-    application.view.style.width = h * (WIDTH / HEIGHT) + 'px'
-    application.view.style.height = h + 'px';
-  } else {
-    application.view.style.width = w + 'px';
-    application.view.style.height = h + 'px';
-  }
-
-
-  //配置路由
-  initRouter(application.stage, { Home, Level, World })
-
-  application.loader.baseUrl = './assets/'
-
-  if (process.env.NODE_ENV == 'development') {
-    debugLoadImages(application)
-  } else {
-    application.loader.add(['sprites.json', 'tile_road.png', 'tile_round.png', 'tile_win.png'])
-  }
-
-  application.loader.load(() => {
-    to('Home')
-  })
-
-  document.body.appendChild(application.view)
+//适配ipad，两边留白
+let w = window.innerWidth
+let h = window.innerHeight
+if (w / h > 0.6) {
+  application.view.style.width = h * (WIDTH / HEIGHT) + 'px'
+  application.view.style.height = h + 'px';
+} else {
+  application.view.style.width = w + 'px';
+  application.view.style.height = h + 'px';
 }
+
+//配置路由
+initRouter(application.stage, { Home, Level, World })
+
+application.loader.baseUrl = './assets/'
+
+if (process.env.NODE_ENV == 'development') {
+  debugLoadImages(application)
+} else {
+  application.loader.add(['sprites.json', 'tile_road.png', 'tile_round.png', 'tile_win.png'])
+}
+
+application.loader.load(() => {
+  to('Home')
+})
+
+document.body.appendChild(application.view)
